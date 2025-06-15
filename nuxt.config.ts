37d63462bf33e3bfff6c964fb9cdad724ed9128f
rainwashed <dementia.aswad@gmail.com>
2025-06-15 14:45:34 -0400

    inityal commit

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui"
+  },
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@hypernym/nuxt-anime',
+    'shadcn-nuxt'
+  ],
+})
---
+++ pages/onboarding.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+<template>
+  <div>
+    <Button class="bg-spotify" ><Icon name="i-simple-icons:spotify" class="inline text-xl" />Authorize for Spotify</Button>
+    <Button class="bg-osu"><Icon name="i-simple-icons:osu" class="inline text-xl"/>Authorize for osu!</Button>
+  </div>
+</template>
